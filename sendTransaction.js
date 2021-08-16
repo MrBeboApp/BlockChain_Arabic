5b24b4a0f445376af6a334e6ce2a305a3158d830
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2021-08-16 13:00:47 +0200

    add top 10 web3 functions

---
+++ sendTransaction.js
@@ -0,0 +1,6 @@
+web3.eth.sendTransaction(
+    {
+        from:"0x1623Be016027505Ea83533E227F9a009D57C2bd4",
+        to:"0xC659c46A20fEB1230F244055CB62B784A8Da7e31",
+        value:web3.utils.toWei('2','ether')
+})
